9f4e76a98f41d25709bdbeb66a102761f1fac62e
Takuya Tsuchida <tsucchi@cpan.org>
2014-05-30 13:19:58 +0000

    Import of TSUCCHI/Otogiri-Plugin-DeleteCascade-0.03 from CPAN.
    gitpan-cpan-distribution: Otogiri-Plugin-DeleteCascade
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         TSUCCHI/Otogiri-Plugin-DeleteCascade-0.03.tar.gz
    gitpan-cpan-author:       TSUCCHI
    gitpan-cpan-maturity:     released

--- lib/Otogiri/Plugin/DeleteCascade.pm
+++ lib/Otogiri/Plugin/DeleteCascade.pm
@@ -3,7 +3,7 @@ use 5.008005;
 use strict;
 use warnings;
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
 use Otogiri;
 use Otogiri::Plugin;
@@ -13,7 +13,6 @@ our @EXPORT = qw(delete_cascade);
 
 sub delete_cascade {
     my ($self, $table_name, $cond_href) = @_;
-    $cond_href = $self->_deflate_param($table_name, $cond_href);
 
     my @parent_rows = $self->select($table_name, $cond_href);
     my $inspector = DBIx::Inspector->new(dbh => $self->dbh);
